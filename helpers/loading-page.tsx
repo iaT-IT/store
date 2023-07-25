86edbb62464417d018a3a13c0b96dc5234a93f7b
bmo4401 <haiminh612a@gmail.com>
2023-07-25 23:00:42 +0700

    store

--- helpers/loading-page.tsx
+++ helpers/loading-page.tsx
@@ -1,28 +1,23 @@
 import { Separator } from '@/components/ui/separator';
 
 const LoadingPage = ({ children }: { children?: React.ReactNode }) => {
-   console.log('🚀 ~ children:', children);
    return (
       <div className="mx-5 md:mx-14">
          <Separator />
 
-         {children ? (
-            children
-         ) : (
-            <div className="grid grid-cols-2 gap-5">
-               <div className="grid grid-cols-4 gap-2">
-                  <div className="col-span-1 bg-slate-300 animate-pulse max-h-full rounded-lg" />
-                  <div className="col-span-3 bg-slate-300 animate-pulse max-h-full rounded-lg" />
-               </div>
-               <div className="w-full aspect-square animate-pulse  rounded-lg flex flex-col gap-2">
-                  <div className="bg-slate-300 w-full rounded-lg h-1/4" />
-                  <div className="bg-slate-300 w-full rounded-lg h-1/6" />
-                  <div className="bg-slate-300 w-full rounded-lg h-1/4" />
-                  <div className="bg-slate-300 w-full rounded-lg h-1/6" />
-                  <div className="bg-slate-300 w-full rounded-lg h-1/12" />
-               </div>
+         <div className="grid grid-cols-2 gap-5">
+            <div className="grid grid-cols-4 gap-2">
+               <div className="col-span-1 bg-slate-300 animate-pulse max-h-full rounded-lg" />
+               <div className="col-span-3 bg-slate-300 animate-pulse max-h-full rounded-lg" />
             </div>
-         )}
+            <div className="w-full aspect-square animate-pulse  rounded-lg flex flex-col gap-2">
+               <div className="bg-slate-300 w-full rounded-lg h-1/4" />
+               <div className="bg-slate-300 w-full rounded-lg h-1/6" />
+               <div className="bg-slate-300 w-full rounded-lg h-1/4" />
+               <div className="bg-slate-300 w-full rounded-lg h-1/6" />
+               <div className="bg-slate-300 w-full rounded-lg h-1/12" />
+            </div>
+         </div>
          <Separator />
 
          <div className="fixed inset-0 w-full h-screen overflow-hidden opacity-50 bg-slate-600 flex items-center justify-center z-40">
